41c78bb982e6d80afe1df9fed773a2fcba7dc12e
Oleg <w.shevchuk.o@gmail.com>
2020-12-23 17:36:33 +0200

    Basic multipage site on nodeJS server

--- server1.js
+++ server1.js
@@ -1,7 +1,34 @@
 const http = require("http");
-
+const fs = require('fs');
+const path = require('path');
 const server = http.createServer(function (req, res) {
-  res.end("<h1>NodeJS server is working</h1>");
+  console.log(req.url);
+
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'multipage_site', 'index.html'), (err, someData) => {
+      if (err) {
+        throw err
+      } else {
+        res.writeHead(
+          200,
+          { 'Contet-Type': 'text/html' }
+        );
+        res.end(someData);
+      }
+    })
+  } else if (req.url === "/content") {
+    fs.readFile(path.join(__dirname, 'multipage_site', 'index2.html'), (err, someData) => {
+      if (err) {
+        throw err
+      } else {
+        res.writeHead(
+          200,
+          { 'Contet-Type': 'text/html' }
+        );
+        res.end(someData);
+      }
+    });
+  }
 });
 
 server.listen(3000, () => {
